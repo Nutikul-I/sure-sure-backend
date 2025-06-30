1acd4a2538be97a6fb8c01f0df6f6a29bb236755
tipyadaa <amyadaarttywork@gmail.com>
2025-06-30 10:16:57 +0700

    refactor: add db tags to SureSureOrderPackage and OrderDetailPost fields for improved database mapping

--- model/orderPackageModel.go
+++ model/orderPackageModel.go
@@ -1,41 +1,41 @@
 package model
 
 type SureSureOrderPackage struct {
-	ID          int     `json:"id"`
-	RefNo       string  `json:"ref_no"`
-	UserID      int     `json:"user_id"`
-	PackageID   int     `json:"package_id"`
-	Price       float64 `json:"price"`
-	Status      string  `json:"status"`
-	CreatedDate string  `json:"created_date"`
-	UpdatedDate string  `json:"updated_date"`
+	ID          int     `json:"id" db:"id"`
+	RefNo       string  `json:"ref_no" db:"refno"`
+	UserID      int     `json:"user_id" db:"userid"`
+	PackageID   int     `json:"package_id" db:"packageid"`
+	Price       float64 `json:"price" db:"price"`
+	Status      string  `json:"status" db:"status"`
+	CreatedDate string  `json:"created_date" db:"createddate"`
+	UpdatedDate string  `json:"updated_date" db:"updateddate"`
 }
 type OrderDetailPost struct {
-	ReferenceNo             string `json:"ReferenceNo"`
-	OrderNo                 string `json:"OrderNo"`
-	MerchantID              int    `json:"MerchantID"`
-	ProductDetail           string `json:"ProductDetail"`
-	Total                   int    `json:"Total"`
-	FeeRate                 int    `json:"FeeRate"`
-	CurrencyRate            int    `json:"CurrencyRate"`
-	CardType                string `json:"CardType"`
-	CardIssuer              string `json:"CardIssuer"`
-	CurrencyCode            string `json:"CurrencyCode"`
-	CustomerEmail           string `json:"CustomerEmail"`
-	Status                  string `json:"Status"`
-	StatusName              string `json:"StatusName"`
-	PostBackUrl             string `json:"PostBackUrl"`
-	PostBackParameters      string `json:"PostBackParameters"`
-	PostBackMethod          string `json:"PostBackMethod"`
-	PostBackCompleted       bool   `json:"PostBackCompleted"`
-	ReturnUrl               string `json:"ReturnUrl"`
-	Installment             string `json:"installment"`
-	InstallmentMode         string `json:"InstallmentMode"`
-	InstallmentInterestRate string `json:"InstallmentInterestRate"`
-	InstallmentMonth        string `json:"InstallmentMonth"`
-	TotalInterestRate       int    `json:"TotalInterestRate"`
-	PaymentPerMonth         int    `json:"PaymentPerMonth"`
-	OrderDateTime           string `json:"OrderDateTime"`
+	ReferenceNo             string `json:"ReferenceNo" `
+	OrderNo                 string `json:"OrderNo" `
+	MerchantID              int    `json:"MerchantID" `
+	ProductDetail           string `json:"ProductDetail" `
+	Total                   int    `json:"Total" `
+	FeeRate                 int    `json:"FeeRate" `
+	CurrencyRate            int    `json:"CurrencyRate" `
+	CardType                string `json:"CardType" `
+	CardIssuer              string `json:"CardIssuer" `
+	CurrencyCode            string `json:"CurrencyCode" `
+	CustomerEmail           string `json:"CustomerEmail" `
+	Status                  string `json:"Status" `
+	StatusName              string `json:"StatusName" `
+	PostBackUrl             string `json:"PostBackUrl "`
+	PostBackParameters      string `json:"PostBackParameters" `
+	PostBackMethod          string `json:"PostBackMethod" `
+	PostBackCompleted       bool   `json:"PostBackCompleted" `
+	ReturnUrl               string `json:"ReturnUrl" `
+	Installment             string `json:"installment" `
+	InstallmentMode         string `json:"InstallmentMode" `
+	InstallmentInterestRate string `json:"InstallmentInterestRate" `
+	InstallmentMonth        string `json:"InstallmentMonth" `
+	TotalInterestRate       int    `json:"TotalInterestRate" `
+	PaymentPerMonth         int    `json:"PaymentPerMonth" `
+	OrderDateTime           string `json:"OrderDateTime" `
 }
 
 var SQL_ORDER_PACKAGE_GET = `SELECT 
